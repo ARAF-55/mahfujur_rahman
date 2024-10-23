b3f2b673f7c94650b62cbd9ab5104962f2fb5a89
Araf_Mahfuj <arafsoloman3734@gmail.com>
2024-10-24 03:32:28 +0600

    added google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,20 @@ import styles from '../../styles/Layout.module.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={styles.layout_class}>
+      <head>
+      {/* Google Analytics - placed immediately after the head tag */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-KXDFK12TJ3"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KXDFK12TJ3');
+            `,
+          }}
+        />
+      </head>
       <body className={styles.layout_class}>
         <div className="content">
             <Navbar />
