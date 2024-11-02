c73120dd653f16a5d5afc7a429b8f2aa5848b02e
Araf_Mahfuj <arafsoloman3734@gmail.com>
2024-11-02 20:38:06 +0600

    Some changes

--- src/app/research/page.js
+++ src/app/research/page.js
@@ -1,22 +1,22 @@
 import {
-  Code_1, Code_2,
-  Code_3, Code_4,
-  Code_5, Code_6,
-  Code_7, Code_8,
+    Code_1, Code_2,
+    Code_3, Code_4,
+    Code_5, Code_6,
+    Code_7, Code_8,
 
-  Headline_1, Headline_2,
-  Headline_3, Headline_4,
-  Headline_5, Headline_6,
-  Headline_7, Headline_8,
+    Headline_1, Headline_2,
+    Headline_3, Headline_4,
+    Headline_5, Headline_6,
+    Headline_7, Headline_8,
 
-  Text_1, Text_2, Text_3,
-  Text_4, Text_5, Text_6,
-  Text_7, Text_8,
+    Text_1, Text_2, Text_3,
+    Text_4, Text_5, Text_6,
+    Text_7, Text_8,
 
 
-  slide_1, slide_2, slide_3,
-  slide_4, slide_5, slide_6,
-  slide_7, slide_8
+    slide_1, slide_2, slide_3,
+    slide_4, slide_5, slide_6,
+    slide_7, slide_8
 } from "../../../Comps/Data";
 
 
@@ -28,12 +28,6 @@ export default function page() {
 
   return (
     <div className={styles.container}>
-        <InfoSection
-            heading={Headline_1} 
-            text={Text_1}
-            codelink= {Code_1} 
-            googleSlidesUrl={slide_1}
-        />
 
         <InfoSection
             heading={Headline_2} 
@@ -47,6 +41,13 @@ export default function page() {
             text={Text_3}
             codelink= {Code_3} 
             googleSlidesUrl={slide_3}
+        />
+
+        <InfoSection
+            heading={Headline_1} 
+            text={Text_1}
+            codelink= {Code_1} 
+            googleSlidesUrl={slide_1}
         />
 
         <InfoSection
